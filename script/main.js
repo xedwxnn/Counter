4edcf9a8f095b0ace805234897ae5fe0c44e4b15
Edwxn <edwinpena0306@gmail.com>
2024-11-07 10:42:18 -0400

    Actualización de función

--- script/main.js
+++ script/main.js
@@ -12,10 +12,16 @@ function decreaseValue() {
     }
 }
 
-function clearValue() {
-    counterValueEl.innerText = 0;
-}
-
 function add10Value() {
     counterValueEl.innerText = parseInt(counterValueEl.innerText) + 10;
+}
+
+function remove10Value() {
+    if(counterValueEl.innerText >= 10) {
+        counterValueEl.innerText = parseInt(counterValueEl.innerText) - 10;
+    }
+}
+
+function clearValue() {
+    counterValueEl.innerText = 0;
 }
